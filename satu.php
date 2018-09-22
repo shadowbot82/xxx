dc3b4bd693ced24f3f0da33d51b8e6c0a1477cc8
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2018-09-23 06:14:30 +0700

    Create satu.php

---
+++ satu.php
@@ -0,0 +1,37 @@
+<?php
+// @author : Shadow Bot
+// Not For Sale 
+// Gunakan Dengan Bijak :)
+function cat(token, $jumlah, $wait){
+    $x = 0; 
+    while($x < $jumlah) {
+		
+		$body = 'token='.$token.'';
+				
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"https://api-siptruk.c9users.io/api.php");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host: api-siptruk.c9users.io","User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"));
+        $server_output = curl_exec ($ch);
+        curl_close ($ch);
+		echo $server_output."\n";
+        sleep($wait);
+        $x++;
+        flush();
+    }
+}
+print "TUYUL COIN NEWS CAT\n\n";
+
+echo "TOKEN?\nInput : ";
+$token = trim(fgets(STDIN));
+echo "Jumlah?\nInput : ";
+$jumlah = trim(fgets(STDIN));
+echo "Jeda? 0-9999999999 (ex:0)\nInput : ";
+$wait = trim(fgets(STDIN));
+$execute = cat($token, $jumlah, $wait);
+print $execute;
+print "DONE ALL SEND\n";
+?>
